b18bbe5e0a35dfe8112e8c9fb1f65967fdbf06bc
Dordo(Giorgio) Dekanovic <djole6789@gmail.com>
2025-10-14 12:36:26 +0200

    feat: implement cart functionality, product details view, and enhance product listing with dynamic data

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product.js');
+const Cart = require('../models/cart.js');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -33,22 +34,39 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+//Todo:PRODUCT DETAILS //
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, (product) => {
     console.log(product);
     console.log('details', prodId);
+    res.render('shop/product-detail', {
+      product: product,
+      docTitle: product.title,
+      path: '/products',
+    }); // sending a response to the client
   });
 };
 
+// TODO: CART //
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', { docTitle: 'Your Cart', path: '/cart' }); // sending a response to the client
 };
 
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('shop/cart');
+};
+
+// TODO: ORDERS //
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', { docTitle: 'Your Orders', path: '/orders' }); // sending a response to the client
 };
 
+// TODO: CHECKOUT //
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', { docTitle: 'Checkout', path: '/checkout' }); // sending a response to the client
 };
